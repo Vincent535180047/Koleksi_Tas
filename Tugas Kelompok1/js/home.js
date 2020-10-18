9dcb6b853d65865368e679e543aa9c891bed090e
VincentMarcelino <70675637+VincentMarcelino@users.noreply.github.com>
2020-10-19 03:12:58 +0700

    ganti latar dan memperbaiki navbar

--- Tugas Kelompok1/js/home.js
+++ Tugas Kelompok1/js/home.js
@@ -7,10 +7,10 @@ $(document).ready(function(){
         var hash = this.hash;
   
         $('html, body').animate({
-          scrollTop: $(hash).offset().top
+          scrollTop: $(hash).offset().top - 100
         }, 800, function(){
      
-          window.location.hash = hash;
+
         });
       }
     });
